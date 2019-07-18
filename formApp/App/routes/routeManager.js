0ebac11e2c701eb93e33423206e8d2d45e6db2d9
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-07-18 19:12:20 +0300

    update

--- formApp/App/routes/routeManager.js
+++ formApp/App/routes/routeManager.js
@@ -1,9 +1,6 @@
 var routes = require('./home');
 var route = require('./login');
 
-
-
-
 module.exports = function(app) {
     app.use('/', routes);
     app.use('/login', routes);
--- formApp/app.js
+++ formApp/app.js
@@ -6,12 +6,12 @@ var expressLayout = require('express-ejs-layouts');
 var db = require('./App/models/db');
 var db = require('./App/models/users');
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.set('views', path.join(__dirname, '/App/views'));
-app.set('view engine', 'ejs');
-app.use(expressLayout);
+app.use(express.static(path.join(__dirname, '/public'))); //public dosyalarını kullanılır hale getirdik.
+app.set('views', path.join(__dirname, '/App/views')); //view aktif ettik
+app.set('view engine', 'ejs'); //view de  ejs kullanak için  aktiflik kullandık.
+app.use(expressLayout); //ejs kullanılır hale geldi.
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); //post dataları yakalamaya yarar.
 
 app.use(bodyParser.json());
 console.log("ddddd");
